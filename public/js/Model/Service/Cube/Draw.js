828855cb56d80c059605df968001baa2bb4dad6b
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-01 10:01:08 +0000

    Omit bottom of cube for now

--- public/js/Model/Service/Cube/Draw.js
+++ public/js/Model/Service/Cube/Draw.js
@@ -5,7 +5,7 @@ Model.Service.Cube.Draw = class {
         var style = 'transform: rotateX(' + cubeEntity.rotateX + 'deg) rotateY(' + cubeEntity.rotateY + 'deg) rotateZ(' + cubeEntity.rotateZ + 'deg) translateX(' + cubeEntity.translateX + 'px) translateY(' + cubeEntity.translateY + 'px) translateZ(' + cubeEntity.translateZ + 'px);';
 
         var cube = $(document.createElement('hcj3d-cube'));
-        cube.append('<hcj3d-cube-side side="bottom"></hcj3d-cube-side>');
+        // cube.append('<hcj3d-cube-side side="bottom"></hcj3d-cube-side>');
         cube.append('<hcj3d-cube-side side="top"></hcj3d-cube-side>');
         cube.append('<hcj3d-cube-side side="left"></hcj3d-cube-side>');
         cube.append('<hcj3d-cube-side side="right"></hcj3d-cube-side>');
