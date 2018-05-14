9ee5be5fd66d372c0cad14fbe118db87fe518405
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-14 02:56:46 +0000

    Loop through all cube entities when detecting object collision

--- public/js/Model/Service/Mannequin/Move.js
+++ public/js/Model/Service/Mannequin/Move.js
@@ -14,12 +14,15 @@ Model.Service.Mannequin.Move = class {
       return false;
     }
 
-    // Does new X run into the first cube?
-    var cubeEntity = cubeEntities[0];
-    if ((currentY > cubeEntity.translateY - 5) && (currentY < cubeEntity.translateY + 95)) {
-      if ((newX > cubeEntity.translateX - 5) && (newX < cubeEntity.translateX + 100 - 5)) {
-        console.log('uhoh1');
-        return false;
+    // Does new X run into a cube?
+    var cubeEntitiesLength = cubeEntities.length;
+    for (var i = 0; i < cubeEntitiesLength; i++) {
+      var cubeEntity = cubeEntities[i];
+      if ((currentY > cubeEntity.translateY - 5) && (currentY < cubeEntity.translateY + 95)) {
+        if ((newX > cubeEntity.translateX - 5) && (newX < cubeEntity.translateX + 100 - 5)) {
+          console.log('uhoh1');
+          return false;
+        }
       }
     }
 
@@ -40,12 +43,15 @@ Model.Service.Mannequin.Move = class {
       return false;
     }
 
-    // Does new Y run into the first cube?
-    var cubeEntity = cubeEntities[0];
-    if ((currentX > cubeEntity.translateX - 5) && (currentX < cubeEntity.translateX + 95)) {
-      if ((newY > cubeEntity.translateY - 5) && (newY < cubeEntity.translateY + 100 - 5)) {
-        console.log('uhoh2');
-        return false;
+    // Does new Y run into a cube?
+    var cubeEntitiesLength = cubeEntities.length;
+    for (var i = 0; i < cubeEntitiesLength; i++) {
+      var cubeEntity = cubeEntities[i];
+      if ((currentX > cubeEntity.translateX - 5) && (currentX < cubeEntity.translateX + 95)) {
+        if ((newY > cubeEntity.translateY - 5) && (newY < cubeEntity.translateY + 100 - 5)) {
+          console.log('uhoh2');
+          return false;
+        }
       }
     }
 
