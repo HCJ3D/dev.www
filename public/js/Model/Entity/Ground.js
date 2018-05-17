9032ecde6578b2c580bb32821ba40bd15d2aae08
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-17 21:27:59 +0000

    Create ground entity

---
+++ public/js/Model/Entity/Ground.js
@@ -0,0 +1,26 @@
+var Model = Model || {};
+Model.Entity = Model.Entity || {};
+
+Model.Entity.Ground = class {
+    constructor(
+        groundId,
+        type,
+        translateX,
+        translateY,
+        translateZ,
+        rotateX,
+        rotateY,
+        rotateZ
+    ) {
+        this.groundId     = groundId;
+        this.translateX = translateX;
+        this.translateY = translateY;
+        this.translateZ = translateZ;
+        this.rotateX    = rotateX;
+        this.rotateY    = rotateY;
+        this.rotateZ    = rotateZ;
+        this.transformOriginX;
+        this.transformOriginY;
+        this.transformOriginZ;
+    }
+}
