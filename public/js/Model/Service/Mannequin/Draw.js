e4f122a5cf352ca3f280e10bec4971ef5927cb43
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-17 22:32:51 +0000

    Hide bottom sides for now for optimization

--- public/js/Model/Service/Mannequin/Draw.js
+++ public/js/Model/Service/Mannequin/Draw.js
@@ -21,7 +21,7 @@ Model.Service.Mannequin.Draw = class {
         mannequin.append('<hcj3d-mannequin-leg class="left"></hcj3d-mannequin-leg>');
         mannequin.children('hcj3d-mannequin-leg.left')
           .append('<hcj3d-mannequin-leg-top></hcj3d-mannequin-leg-top>')
-          .append('<hcj3d-mannequin-leg-bottom></hcj3d-mannequin-leg-bottom>')
+          // .append('<hcj3d-mannequin-leg-bottom></hcj3d-mannequin-leg-bottom>')
           .append('<hcj3d-mannequin-leg-left></hcj3d-mannequin-leg-left>')
           .append('<hcj3d-mannequin-leg-right></hcj3d-mannequin-leg-right>')
           .append('<hcj3d-mannequin-leg-front></hcj3d-mannequin-leg-front>')
@@ -30,7 +30,7 @@ Model.Service.Mannequin.Draw = class {
         mannequin.append('<hcj3d-mannequin-leg class="right"></hcj3d-mannequin-leg>');
         mannequin.children('hcj3d-mannequin-leg.right')
           .append('<hcj3d-mannequin-leg-top></hcj3d-mannequin-leg-top>')
-          .append('<hcj3d-mannequin-leg-bottom></hcj3d-mannequin-leg-bottom>')
+          // .append('<hcj3d-mannequin-leg-bottom></hcj3d-mannequin-leg-bottom>')
           .append('<hcj3d-mannequin-leg-left></hcj3d-mannequin-leg-left>')
           .append('<hcj3d-mannequin-leg-right></hcj3d-mannequin-leg-right>')
           .append('<hcj3d-mannequin-leg-front></hcj3d-mannequin-leg-front>')
@@ -39,7 +39,7 @@ Model.Service.Mannequin.Draw = class {
         mannequin.append('<hcj3d-mannequin-body></hcj3d-mannequin-body>');
         mannequin.children('hcj3d-mannequin-body')
           .append('<hcj3d-mannequin-body-top></hcj3d-mannequin-body-top>')
-          .append('<hcj3d-mannequin-body-bottom></hcj3d-mannequin-body-bottom>')
+          // .append('<hcj3d-mannequin-body-bottom></hcj3d-mannequin-body-bottom>')
           .append('<hcj3d-mannequin-body-left></hcj3d-mannequin-body-left>')
           .append('<hcj3d-mannequin-body-right></hcj3d-mannequin-body-right>')
           .append('<hcj3d-mannequin-body-front></hcj3d-mannequin-body-front>')
@@ -48,7 +48,7 @@ Model.Service.Mannequin.Draw = class {
         mannequin.append('<hcj3d-mannequin-head></hcj3d-mannequin-head>');
         mannequin.children('hcj3d-mannequin-head')
           .append('<hcj3d-mannequin-head-top></hcj3d-mannequin-head-top>')
-          .append('<hcj3d-mannequin-head-bottom></hcj3d-mannequin-head-bottom>')
+          // .append('<hcj3d-mannequin-head-bottom></hcj3d-mannequin-head-bottom>')
           .append('<hcj3d-mannequin-head-left></hcj3d-mannequin-head-left>')
           .append('<hcj3d-mannequin-head-right></hcj3d-mannequin-head-right>')
           .append('<hcj3d-mannequin-head-front></hcj3d-mannequin-head-front>')
